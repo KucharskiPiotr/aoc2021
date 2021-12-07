a0fd12177e9b193d8abebc1264f4562f2fdf73da
KucharskiPiotr <piotr-kucharski@outlook.com>
2021-12-07 22:03:36 +0100

    day 2 part 1

--- day2.hs
+++ day2.hs
@@ -1,25 +1,33 @@
-import System.IO
-import Control.Monad
+module Main where
+  import System.IO
+  import Control.Monad
+  import Data.List.Split
 
-data Command = Forward | Up | Down
+  data Command = Forward | Up | Down
 
-navigate :: (Int, Int) -> Command -> (Int, Int)
-navigate (horizontal, depth) command = case command of
-  Forward -> (horizontal + 1, depth)
-  Down -> (horizontal, depth + 1)
-  Up -> (horizontal, depth - 1)
+  navigate :: (Int, Int) -> (Maybe Command, Int) -> (Int, Int)
+  navigate (horizontal, depth) (command, amount) = case command of
+    Nothing -> (horizontal, depth)
+    Just c -> case c of
+      Forward -> (horizontal + amount, depth)
+      Down -> (horizontal, depth + amount)
+      Up -> (horizontal, depth - amount)
 
-adaptCommand :: String -> Maybe Command
-adaptCommand command = case command of
-    "forward" -> Just Forward
-    "down" -> Just Down
-    "up" -> Just Up
-    _ -> Nothing  
+  adaptCommand :: String -> Maybe Command
+  adaptCommand command = case command of
+      "forward" -> Just Forward
+      "down" -> Just Down
+      "up" -> Just Up
+      _ -> Nothing  
 
-main :: IO()
-main = do
-    handle <- openFile "day2.txt" ReadMode
-    contents <- hGetContents handle
-    let entries = lines contents
-    
-    hClose handle
+  parseCommandLine :: String -> (Maybe Command, Int)
+  parseCommandLine txt = (adaptCommand(head(splitOn " " txt)), read (last(splitOn " " txt)) :: Int)
+
+  main :: IO()
+  main = do
+      handle <- openFile "day2.txt" ReadMode
+      contents <- hGetContents handle
+      let commands = map parseCommandLine $ lines contents
+      let res = foldl navigate (0, 0) commands
+      print res
+      hClose handle
