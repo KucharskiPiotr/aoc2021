6500b75be49b51ee9d121fb12e677c40fcd30f13
KucharskiPiotr <piotr-kucharski@outlook.com>
2021-12-07 22:13:18 +0100

    Day 2 finished

--- day2.hs
+++ day2.hs
@@ -13,6 +13,14 @@ module Main where
       Down -> (horizontal, depth + amount)
       Up -> (horizontal, depth - amount)
 
+  navigateAim :: (Int, Int, Int) -> (Maybe Command, Int) -> (Int, Int, Int)
+  navigateAim (horizontal, depth, aim) (command, amount) = case command of
+    Nothing -> (horizontal, depth, aim)
+    Just c -> case c of
+      Forward -> (horizontal + amount, depth + (aim * amount), aim)
+      Down -> (horizontal, depth, aim + amount)
+      Up -> (horizontal, depth, aim - amount)
+
   adaptCommand :: String -> Maybe Command
   adaptCommand command = case command of
       "forward" -> Just Forward
@@ -29,5 +37,14 @@ module Main where
       contents <- hGetContents handle
       let commands = map parseCommandLine $ lines contents
       let res = foldl navigate (0, 0) commands
+      print res
+      hClose handle
+
+  mainPart2 :: IO()
+  mainPart2 = do
+      handle <- openFile "day2.txt" ReadMode
+      contents <- hGetContents handle
+      let commands = map parseCommandLine $ lines contents
+      let res = foldl navigateAim (0, 0, 0) commands
       print res
       hClose handle
