9856f5b80b6bb90ce89a45728f596a4953a2d56c
KucharskiPiotr <piotr-kucharski@outlook.com>
2021-12-12 16:54:05 +0100

    Day3 completed

--- day3.hs
+++ day3.hs
@@ -1,7 +1,8 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-import System.IO
+
 import Control.Monad
 import Data.List.Split
+import System.IO
 
 addBitValues :: [Int] -> String -> [Int]
 addBitValues [] "" = []
@@ -11,23 +12,23 @@ addBitValues (latestSum : remainingSum) (latestBit : remainingBits) =
     '1' -> (latestSum + 1) : addBitValues remainingSum remainingBits
     _ -> latestSum : remainingSum
 
-bitListToInt :: [Int] -> Int 
+bitListToInt :: [Int] -> Int
 bitListToInt [] = 0
 bitListToInt bits = last bits + 2 * bitListToInt (init bits)
 
 generateParameter :: (Int -> Bool) -> [Int] -> Int
-generateParameter predicate bits = 
-  bitListToInt $ map (\ b -> if predicate b then 1 else 0) bits
+generateParameter predicate bits =
+  bitListToInt $ map (\b -> if predicate b then 1 else 0) bits
 
 generateGamma :: Int -> [Int] -> Int
-generateGamma threshold = generateParameter (>threshold)
+generateGamma threshold = generateParameter (> threshold)
 
 generateEpsilon :: Int -> [Int] -> Int
-generateEpsilon threshold = generateParameter (<threshold)
+generateEpsilon threshold = generateParameter (< threshold)
 
-main :: IO()
+main :: IO ()
 main = do
-  handle <- openFile "day3.txt" ReadMode 
+  handle <- openFile "day3.txt" ReadMode
   contents <- hGetContents handle
   let numberLines = lines contents
   let amountOfLines = length numberLines
@@ -43,37 +44,51 @@ main = do
 
 bitSum :: String -> Int
 bitSum "" = 0
-bitSum (b:bs) = case b of
+bitSum (b : bs) = case b of
   '0' -> bitSum bs
   '1' -> 1 + bitSum bs
   _ -> bitSum bs
 
-getOxygenRating :: Int -> [String] -> String
-getOxygenRating _ [result] = result
-getOxygenRating index numberStrings = do
-  let sum = bitSum $ map (!!index) numberStrings
-  getOxygenRating (index + 1) $ 
-    filter (\ns -> 
-      (sum >= (length numberStrings `div` 2) && ns !! index == '1') || 
-      (sum < (length numberStrings `div` 2) && ns !! index == '0')) numberStrings
+getRating :: (Char -> Bool) -> (Char -> Bool) -> (Char -> Bool) -> Int -> [String] -> String
+getRating _ _ _ _ [result] = result
+getRating whenMoreOnes whenMoreZeros whenEqual index numberStrings = do
+  let doubleSum = (*) 2 $ bitSum $ map (!! index) numberStrings
+  let numberCount = length numberStrings
+  getRating whenMoreOnes whenMoreZeros whenEqual (index + 1) $
+    filter
+      ( \ns ->
+          (doubleSum > numberCount && whenMoreOnes (ns !! index))
+            || (doubleSum < numberCount && whenMoreZeros (ns !! index))
+            || (doubleSum == numberCount && whenEqual (ns !! index))
+      )
+      numberStrings
 
+getOxygenRating :: Int -> [String] -> String
+getOxygenRating =
+  getRating
+    (== '1')
+    (== '0')
+    (== '1')
 
 getCo2Rating :: Int -> [String] -> String
-getCo2Rating _ [result] = result
-getCo2Rating index numberStrings = do
-  let sum = bitSum $ map (!!index) numberStrings
-  getOxygenRating (index + 1) $ 
-    filter (\ns -> 
-      (sum >= (length numberStrings `div` 2) && ns !! index == '0') || 
-      (sum < (length numberStrings `div` 2) && ns !! index == '1')) numberStrings
+getCo2Rating = 
+  getRating
+    (== '0')
+    (== '1')
+    (== '0')
 
-mainPart2 :: IO()
+binToInt :: String -> Int
+binToInt binary =
+  bitListToInt (map (read . pure :: Char -> Int) binary)
+
+mainPart2 :: IO ()
 mainPart2 = do
-  handle <- openFile "day3.txt" ReadMode 
+  handle <- openFile "day3.txt" ReadMode
   contents <- hGetContents handle
   let numberLines = lines contents
-  let res = getOxygenRating 0 numberLines
-  let co2 = getCo2Rating 0 numberLines
+  let res = binToInt $ getOxygenRating 0 numberLines
+  let co2 = binToInt $ getCo2Rating 0 numberLines
   print res
   print co2
+  print (res * co2)
   hClose handle
